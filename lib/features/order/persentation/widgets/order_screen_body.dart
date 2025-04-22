3fa3c426b299bd4c4461099beedb0f677a79fc71
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 11:58:00 +0300

    create OrderScreenBody

--- lib/features/order/persentation/widgets/order_screen_body.dart
+++ lib/features/order/persentation/widgets/order_screen_body.dart
@@ -1,13 +1,16 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test_project/features/order/persentation/widgets/custom_app_bar.dart';
-import 'package:flutter_test_project/features/order/persentation/widgets/cutsom_text_feild.dart';
 import 'package:flutter_test_project/features/order/persentation/widgets/text_feilds_list_view.dart';
 
-class OrderScreen extends StatelessWidget {
-  const OrderScreen({super.key});
+class OrderScreenBody extends StatelessWidget {
+  const OrderScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: OrderScreenBody());
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [CustomAppBar(), Expanded(child: TextFeildsListView())],
+    );
   }
 }
