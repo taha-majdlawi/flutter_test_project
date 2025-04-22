339a65555201d442026010b2bac0e57ec2c8a9a6
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 11:51:28 +0300

    refactor CustomTextFeilds and add second textFeild

--- lib/features/order/persentation/order_screen.dart
+++ lib/features/order/persentation/order_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test_project/core/utils/assets.dart';
 import 'package:flutter_test_project/core/widgets/order_number_text.dart';
 import 'package:flutter_test_project/features/order/persentation/widgets/custom_app_bar.dart';
-import 'package:flutter_test_project/features/order/persentation/widgets/left_custom_text_feild.dart';
+import 'package:flutter_test_project/features/order/persentation/widgets/cutsom_text_feild.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
@@ -13,10 +13,7 @@ class OrderScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          LeftCustomTextFeild(),
-        ],
+        children: [CustomAppBar(), CustomTextFeilds()],
       ),
     );
   }
---
+++ lib/features/order/persentation/widgets/cutsom_text_feild.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFeilds extends StatelessWidget {
+  const CustomTextFeilds({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width * 0.15,
+          decoration: BoxDecoration(
+            border: Border(
+              right: BorderSide(color: Color(0xff006B83), width: 2),
+            ),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xff006B83), width: 2),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xff006B83), width: 2),
+              ),
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            child: TextField(
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff006B83), width: 2),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff006B83), width: 2),
+                ),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
