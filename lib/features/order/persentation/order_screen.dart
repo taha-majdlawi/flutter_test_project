a86b352590d579fbcd28cee96bef45b4174273fc
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 11:58:17 +0300

    refactor

--- lib/features/order/persentation/order_screen.dart
+++ lib/features/order/persentation/order_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_project/features/order/persentation/widgets/custom_app_bar.dart';
 import 'package:flutter_test_project/features/order/persentation/widgets/cutsom_text_feild.dart';
+import 'package:flutter_test_project/features/order/persentation/widgets/order_screen_body.dart';
 import 'package:flutter_test_project/features/order/persentation/widgets/text_feilds_list_view.dart';
 
 class OrderScreen extends StatelessWidget {
