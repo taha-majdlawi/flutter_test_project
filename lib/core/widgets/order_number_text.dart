0b05ad04a2a8640bf9c19229f7daf5a10639f102
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 11:00:37 +0300

    create CustomOrderNumberText

---
+++ lib/core/widgets/order_number_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomOrderNumberText extends StatelessWidget {
+  const CustomOrderNumberText({super.key, required this.orderNumber});
+  final int orderNumber;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Order# ',
+          style: TextStyle(
+            color: Color(0xff5000B9),
+            fontWeight: FontWeight.w600,
+            fontSize: 16,
+          ),
+        ),
+        Text(
+          '$orderNumber',
+          style: TextStyle(
+            color: Color(0xff006B83),
+            fontWeight: FontWeight.w600,
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/order/persentation/order_screen.dart
+++ lib/features/order/persentation/order_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test_project/core/utils/assets.dart';
+import 'package:flutter_test_project/core/widgets/order_number_text.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
@@ -16,19 +17,26 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 45.0, left: 17),
-          child: Image.asset(AppAssets.drawer, width: 31, height: 30),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 107.0, top: 50),
-          child: Image.asset(AppAssets.logo, width: 123, height: 123),
-        ),
-
-        Text('Oerder#')
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 17.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 45.0),
+                child: Image.asset(AppAssets.drawer, width: 31, height: 30),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 107.0, top: 50),
+                child: Image.asset(AppAssets.logo, width: 123, height: 123),
+              ),
+            ],
+          ),
+          CustomOrderNumberText(orderNumber: 112096),
+        ],
+      ),
     );
   }
 }
