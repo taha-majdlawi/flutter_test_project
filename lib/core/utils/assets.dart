42bf573d51e7de15533dda20ab802ba656b49fc7
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 12:10:20 +0300

    add icon images to AppAssets class

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,7 @@
 class AppAssets {
   static const String logo = 'assets/images/logo.png';
   static const String drawer = 'assets/images/drawer.png';
+  static const String arrow_forword = 'assets/images/arrow_forward.png';
+  static const String arrow_back = 'assets/images/arrow_back.png';
+  static const String cancel = 'assets/images/cancel.png';
 }
