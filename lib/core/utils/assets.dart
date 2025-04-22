77651264392ff60b9ff1a70e3e848d075344a5dd
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 12:22:49 +0300

    refactor custom appBar

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,4 @@
-class AppAssets {
+class AppAssets {             
   static const String logo = 'assets/images/logo.png';
   static const String drawer = 'assets/images/drawer.png';
   static const String arrow_forword = 'assets/images/arrow_forward.png';
--- lib/features/order/persentation/widgets/custom_app_bar.dart
+++ lib/features/order/persentation/widgets/custom_app_bar.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test_project/core/utils/assets.dart';
 import 'package:flutter_test_project/core/widgets/order_number_text.dart';
@@ -17,21 +15,29 @@ class CustomAppBar extends StatelessWidget {
           Row(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 45.0),
+                padding: const EdgeInsets.only(top: 40.0),
                 child: Image.asset(AppAssets.drawer, width: 31, height: 30),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 107.0, top: 50),
-                child: Image.asset(AppAssets.logo, width: 123, height: 123),
+                padding: const EdgeInsets.only(left: 95.0, top: 50),
+                child: Image.asset(AppAssets.logo, width: 123, height: 80),
               ),
             ],
           ),
-          CustomOrderNumberText(orderNumber: 112096),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              
+              Padding(
+                padding: const EdgeInsets.only(left: 50.0),
+                child: Image.asset(AppAssets.cancel, width: 24, height: 24),
+              ),
+              Image.asset(AppAssets.arrow_forword, width: 24, height: 24),
             ],
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: CustomOrderNumberText(orderNumber: 112096),
+          ),
         ],
       ),
     );
