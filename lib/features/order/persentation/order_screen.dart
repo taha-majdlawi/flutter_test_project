d180c553e89e97242c539493afed4742747850a2
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 10:20:21 +0300

    add app logo and create custom order app bar

--- lib/features/order/persentation/order_screen.dart
+++ lib/features/order/persentation/order_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_project/core/utils/assets.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: Column(children: [CustomAppBar()]));
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [Image.asset(AppAssets.logo)]);
   }
 }
