681e653ff1d45274904ac6cb540c5475d20be804
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 11:06:38 +0300

    refactor

--- lib/features/order/persentation/order_screen.dart
+++ lib/features/order/persentation/order_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test_project/core/utils/assets.dart';
 import 'package:flutter_test_project/core/widgets/order_number_text.dart';
+import 'package:flutter_test_project/features/order/persentation/widgets/custom_app_bar.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
@@ -9,34 +10,5 @@ class OrderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Column(children: [CustomAppBar()]));
-  }
-}
-
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 17.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 45.0),
-                child: Image.asset(AppAssets.drawer, width: 31, height: 30),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 107.0, top: 50),
-                child: Image.asset(AppAssets.logo, width: 123, height: 123),
-              ),
-            ],
-          ),
-          CustomOrderNumberText(orderNumber: 112096),
-        ],
-      ),
-    );
   }
 }
