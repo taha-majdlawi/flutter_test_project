cb79215d90025fd088797c84f42f7d3bc691635e
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-22 11:26:30 +0300

    create LeftCustomTextFeild

--- lib/features/order/persentation/order_screen.dart
+++ lib/features/order/persentation/order_screen.dart
@@ -3,12 +3,15 @@ import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test_project/core/utils/assets.dart';
 import 'package:flutter_test_project/core/widgets/order_number_text.dart';
 import 'package:flutter_test_project/features/order/persentation/widgets/custom_app_bar.dart';
+import 'package:flutter_test_project/features/order/persentation/widgets/left_custom_text_feild.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [CustomAppBar()]));
+    return Scaffold(
+      body: Column(children: [CustomAppBar(), LeftCustomTextFeild()]),
+    );
   }
 }
