c5f392f109676e6aa4a7e869421e020a217c6c66
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-23 11:09:41 +0300

    create OrderSettingScreen

---
+++ lib/features/order_setting/persentation/order_setting_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class OrderSettingScreen extends StatelessWidget {
+  const OrderSettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
