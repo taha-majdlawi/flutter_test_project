80138b9eb33154e2a2a7f74552196040ebf11177
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-24 13:25:51 +0300

    create OrderSettingScreenBody

--- lib/features/order_setting/persentation/order_setting_screen.dart
+++ lib/features/order_setting/persentation/order_setting_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test_project/core/utils/styles.dart';
 import 'package:flutter_test_project/core/widgets/custom_text_wiidget.dart';
 import 'package:flutter_test_project/features/order_setting/persentation/widgets/custom_app_bar.dart';
 import 'package:flutter_test_project/features/order_setting/persentation/widgets/order_info.dart';
+import 'package:flutter_test_project/features/order_setting/persentation/widgets/order_setting_screen_body.dart';
 
 
 class OrderSettingScreen extends StatelessWidget {
@@ -12,23 +13,7 @@ class OrderSettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 44.0),
-            child: CustomOrderSettingScreenAppBar(),
-          ),
-          Padding(padding: const EdgeInsets.all(20.0), child: OrderInfo()),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: const CustomTextWidget(
-              text: 'Delivery instructions ...',
-              style: Styles.blueTextStyle16,
-            ),
-          ),
-        ],
-      ),
+      body: OrderSettingScreenBody(),
     );
   }
 }
---
+++ lib/features/order_setting/persentation/widgets/order_setting_screen_body.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_project/core/utils/styles.dart';
+import 'package:flutter_test_project/core/widgets/custom_text_wiidget.dart';
+import 'package:flutter_test_project/features/order_setting/persentation/widgets/custom_app_bar.dart';
+import 'package:flutter_test_project/features/order_setting/persentation/widgets/order_info.dart';
+
+class OrderSettingScreenBody extends StatelessWidget {
+  const OrderSettingScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 44.0),
+          child: CustomOrderSettingScreenAppBar(),
+        ),
+        Padding(padding: const EdgeInsets.all(20.0), child: OrderInfo()),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: const CustomTextWidget(
+            text: 'Delivery instructions ...',
+            style: Styles.blueTextStyle16,
+          ),
+        ),
+      ],
+    );
+  }
+}
