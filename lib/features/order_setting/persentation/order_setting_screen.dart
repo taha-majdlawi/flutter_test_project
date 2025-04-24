8b0e5fdb909aa88f09b217d1c0671cd768bad27c
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-24 13:26:16 +0300

    refactor

--- lib/features/order_setting/persentation/order_setting_screen.dart
+++ lib/features/order_setting/persentation/order_setting_screen.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_test_project/core/utils/styles.dart';
-import 'package:flutter_test_project/core/widgets/custom_text_wiidget.dart';
-import 'package:flutter_test_project/features/order_setting/persentation/widgets/custom_app_bar.dart';
-import 'package:flutter_test_project/features/order_setting/persentation/widgets/order_info.dart';
 import 'package:flutter_test_project/features/order_setting/persentation/widgets/order_setting_screen_body.dart';
 
 
