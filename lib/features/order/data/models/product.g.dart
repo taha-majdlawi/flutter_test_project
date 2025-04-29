1568416cddefdcad3167fbe927e3978d3ff04ce9
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-29 16:46:20 +0300

    run flutter builder

---
+++ lib/features/order/data/models/product.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Product _$ProductFromJson(Map<String, dynamic> json) => Product(
+      id: json['id'] as String,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$ProductToJson(Product instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
