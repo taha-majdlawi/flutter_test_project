495db7f0dbc0be04dfd1f9f4fdfc722139357f9b
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-29 16:32:14 +0300

    create product model nad product repo

---
+++ lib/features/order/data/models/product.dart
@@ -0,0 +1,6 @@
+class Product {
+  final String id;
+  final String name;
+
+  Product({required this.id, required this.name});
+}
---
+++ lib/features/order/data/repos/product_client.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test_project/features/order/data/models/product.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+
+part 'product_client.g.dart';
+
+@RestApi(baseUrl: "https://app.giotheapp.com/api-sample/")
+abstract class ProductClient {
+  factory ProductClient(Dio dio, {String baseUrl}) = _ProductClient;
+
+  @GET("")
+  Future<Product> getProducts();
+}
