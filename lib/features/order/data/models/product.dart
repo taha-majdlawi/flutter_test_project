185a780c16d2d2c2d1b3e08781349c280f39d265
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-04-29 16:43:54 +0300

    fix file name have extra point

--- lib/features/order/data/models/product.dart
+++ lib/features/order/data/models/product.dart
@@ -1,6 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'product.g.dart';
+
+@JsonSerializable()
 class Product {
   final String id;
   final String name;
 
   Product({required this.id, required this.name});
+
+  factory Product.fromJson(Map<String, dynamic> json) => _$ProductFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProductToJson(this);
 }
--- lib/features/order_setting/persentation/widgets/order_info.dart
+++ lib/features/order_setting/persentation/widgets/order_info.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test_project/features/order_setting/persentation/widgets/custom_order_info_left_side..dart';
+import 'package:flutter_test_project/features/order_setting/persentation/widgets/custom_order_info_left_side.dart';
 import 'package:flutter_test_project/features/order_setting/persentation/widgets/custom_order_info_right_side.dart';
 
 class OrderInfo extends StatelessWidget {
